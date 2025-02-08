f5488d07dbe08f05fa0a361d67c38450374f3b38
Sudeepramasamy <sudeep9568@gmail.com>
2025-02-08 17:20:05 +0530

    Collected static files and updated project

--- web_project/web_project/settings.py
+++ web_project/web_project/settings.py
@@ -121,8 +121,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL='/images/'
-MEDIA_ROOT=BASE_DIR/'static'
+MEDIA_URL='/media/'
+MEDIA_ROOT=BASE_DIR/'media'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
